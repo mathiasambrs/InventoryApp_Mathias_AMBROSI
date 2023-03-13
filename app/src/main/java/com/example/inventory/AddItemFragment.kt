f442c64ebce7c5075bfdab39d0a5a21af1ef8d1d
mathiasambrs <mathias.ambrs@eduge.ch>
2023-03-13 14:49:57 +0100

    Ex 9 part 2

--- app/src/main/java/com/example/inventory/AddItemFragment.kt
+++ app/src/main/java/com/example/inventory/AddItemFragment.kt
@@ -74,6 +74,12 @@ class AddItemFragment : Fragment() {
             findNavController().navigate(action)
         }
     }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.saveAction.setOnClickListener {
+            addNewItem()
+        }
+    }
     /**
      * Called before fragment is destroyed.
      */
